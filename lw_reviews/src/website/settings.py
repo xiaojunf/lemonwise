fd9235d35f20d99eb11abf2c843dbd7b33dd8f5a
Xiaojun Feng <junjun80585@gmail.com>
2011-07-20 11:45:43 -0700

    test

--- lw_reviews/src/website/settings.py
+++ lw_reviews/src/website/settings.py
@@ -13,9 +13,17 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'lemonwise_v1.db',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'lemonwise_v1.db',                      # Or path to database file if using sqlite3.
+        'USER': 'login',                      # Not used with sqlite3.
+        'PASSWORD': '870801',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'lemonwise_v1.db',
+#    }
 }
 
 APPS_ROOT = realpath(join(dirname(__file__), '..', 'lemonwise'))
